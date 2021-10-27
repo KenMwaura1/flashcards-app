034714e4c2daf556f0062f031cc5481e63efa2d6
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-27 10:07:34 +0300

    generated a note card and styles the search title bar.

--- src/frontend/flashcard/src/app/app.module.ts
+++ src/frontend/flashcard/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
     LayoutComponent,
+    NoteCardComponent,
   ],
   imports: [
     BrowserModule,
