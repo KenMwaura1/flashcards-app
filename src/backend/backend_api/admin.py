1a3d8c9c1b06536e8e2b5f61aff363e42c2a9822
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-27 12:03:24 +0300

    restructure app

--- src/backend/backend_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Card, Deck
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Card)
-admin.site.register(Deck)
